8297ca09ebae831ce446ac365940eb4d476517ad
radyvoniuk <radyvoniukkate@gmail.com>
2024-08-07 17:13:59 +0300

    fixing contact filtration

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,24 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import { selectContacts } from "/src/redux/contactsSlice.js";
+import { selectNameFilter } from "/src/redux/filtersSlice.js";
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
 
   return (
     <ul className="list">
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
